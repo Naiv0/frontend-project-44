c50cd6bb5a670f9c69240c297fb86193d95e182c
Демид Хабаров <piratnabortu@gmail.com>
2023-03-21 12:21:48 +0300

    maijor refactoring

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { gameStart } from '../scr/index.js';
+import hello from '../scr/hello.js';
 
-gameStart();
+hello();
--- scr/hello.js
+++ scr/hello.js
@@ -1,10 +1,12 @@
 import nameAsk from './cli.js';
 
-const hello = (description) => {
+const hello = (description = null) => {
   console.log('Welcome to the Brain Games!');
   const name = nameAsk();
   console.log(`Hello, ${name}!`);
-  console.log(`${description}`);
+  if (description !== null) {
+    console.log(description);
+  }
   return name;
 };
 export default hello;
